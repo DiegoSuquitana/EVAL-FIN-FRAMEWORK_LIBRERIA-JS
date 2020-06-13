56c0c6dce35293642479fe5d16b4e83b2cb54f75
DiegoSuquitana <35553620+DiegoSuquitana@users.noreply.github.com>
2020-06-13 15:39:34 -0500

    Update app.js
    Llenar tablero 1

--- js/app.js
+++ js/app.js
@@ -20,5 +20,34 @@ function color2(){
 
 $(function(){
     color1();
+    
+    $(".btn-reinicio").click(function(){
+        tablero();
+    })
+
+
+//llenar tablero//
+function tablero(){
+    
+    var numero = 0;
+    var imagen;
+
+    $('.elemento').draggable({disabled: true});
+    
+    for(var i=1; i<8; i++){
+        for(var j=1; j<8; j++){
+            if($('.col-'+j).children("img:nth-child("+i+")").html()==null){
+                numero = Math.floor(Math.random()*4)+ 1;
+                imagen="image/"+numero+".png";
+                $(".col-"+j).prepend("<img src="+imagen+" class='elemento'/>").css("justify-content","flex-start");
+                
+            }
+        }
+    }
+
+
+}
+
+// fin - llenar tablero//
 
 });
